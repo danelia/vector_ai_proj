47795390e4e55098c5848dcfdecfb9924cba870b
danelia <gdane14@freeuni.edu.ge>
2021-10-19 16:41:09 +0400

    added triggers script for sql

--- src/triggers.sql
+++ src/triggers.sql
@@ -10,24 +10,32 @@ DECLARE
 BEGIN
 
 	SELECT INTO curr_population, curr_area
-				SUM(country_population), SUM(country_area)
-                FROM app_country as c
-                WHERE c.continent_id = NEW.continent_id AND c.country_id != NEW.country_id;
+				SUM(c.population), SUM(c.area)
+        FROM app_country as c
+        WHERE c.continent_id = NEW.continent_id AND c.id != NEW.id;
 				
 	SELECT INTO max_population, max_area
-				continent_population, continent_area
+				c.population, c.area
 				FROM app_continent as c
-				WHERE c.continent_id = NEW.continent_id;
+				WHERE c.id = NEW.continent_id;
+				
+	IF curr_population is NULL then
+		curr_population := 0;
+	end if;
+	
+	if curr_area is NULL then
+		curr_area := 0;
+	end if;	
     
-    IF curr_population + NEW.country_population > max_population THEN
-        RAISE EXCEPTION 'Country populations cannot be more than continent population';
-    END IF;
+  IF curr_population + NEW.population > max_population THEN
+    RAISE EXCEPTION 'Country populations cannot be more than continent population';
+  END IF;
 
-    IF curr_area + NEW.country_area > max_area THEN
-        RAISE EXCEPTION 'Country areas cannot be more than continent area';
-    END IF;
+  IF curr_area + NEW.area > max_area THEN
+    RAISE EXCEPTION 'Country areas cannot be more than continent area';
+  END IF;
 
-    RETURN NEW;
+  RETURN NEW;
 END;
 $check_country$ LANGUAGE plpgsql;
 
@@ -51,24 +59,32 @@ DECLARE
 BEGIN
 
 	SELECT INTO curr_population, curr_area
-				SUM(city_population), SUM(city_area)
-                FROM app_city as c
-                WHERE c.country_id = NEW.country_id AND c.city_id != NEW.city_id;
+				SUM(c.population), SUM(c.area)
+        FROM app_city as c
+        WHERE c.country_id = NEW.country_id AND c.id != NEW.id;
 				
 	SELECT INTO max_population, max_area
-				country_population, country_area
+				c.population, c.area
 				FROM app_country as c
-				WHERE c.country_id = NEW.country_id;
+				WHERE c.id = NEW.country_id;
+
+  IF curr_population is NULL then
+		curr_population := 0;
+	end if;
+	
+	if curr_area is NULL then
+		curr_area := 0;
+	end if;	
     
-    IF curr_population + NEW.city_population > max_population THEN
-        RAISE EXCEPTION 'City populations cannot be more than country population';
-    END IF;
+  IF curr_population + NEW.population > max_population THEN
+    RAISE EXCEPTION 'City populations cannot be more than country population';
+  END IF;
 
-    IF curr_area + NEW.city_area > max_area THEN
-        RAISE EXCEPTION 'City areas cannot be more than country area';
-    END IF;
+  IF curr_area + NEW.area > max_area THEN
+    RAISE EXCEPTION 'City areas cannot be more than country area';
+  END IF;
 
-    RETURN NEW;
+  RETURN NEW;
 END;
 $check_city$ LANGUAGE plpgsql;
 
